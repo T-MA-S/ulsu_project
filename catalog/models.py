a996d50ea3289d6d72840c789275679c6bd6446b
T-MA-S <tmas2003@gmail.com>
2022-09-20 11:03:42 +0400

    add UserModel table(add migrations)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class UserModel(AbstractUser):
+    username = None #удаление первичного ключа из родительской модели AbstractUser
+
+    email = models.EmailField('Email', unique=True)
+
+    username = models.TextField("username")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
